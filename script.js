5115fdcfb0e37bf54c0032e5655ce03ac5129a13
BBD31 <daydayda7@gmail.com>
2026-01-31 13:51:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('Redict2')?.addEventListener('click', () => {
+    window.location.href = 'index.html';
+  });
+  document.getElementById('Redict1')?.addEventListener('click', () => {
+    window.location.href = 'Tutorials.html';
+  });
+});
